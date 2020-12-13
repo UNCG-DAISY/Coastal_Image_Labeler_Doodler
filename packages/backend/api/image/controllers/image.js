b5477b8f97fe9cf0011cc3319aa0ce14142f019e
Nafis <nafis.shah@gmail.com>
2020-12-13 13:10:06 -0500

    create many

--- packages/backend/api/image/controllers/image.js
+++ packages/backend/api/image/controllers/image.js
@@ -1,8 +1,35 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    createMany: async ctx => {
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.image.create(data, { files });
+        } else {
+            if (Array.isArray(ctx.request.body)) {
+                //  if array, loop and return created content.  
+                entity = []
+                
+                for (const entry of ctx.request.body) {
+                    const data = await strapi.services.image.create(entry);
+                    entity.push(data)
+                }
+            // return 'X array of content created';
+            } else {
+                entity = await strapi.services.image.create(ctx.request.body);
+            }
+            
+        }
+        return sanitizeEntity(entity, { model: strapi.models.image });
+
+        
+    }
+};
