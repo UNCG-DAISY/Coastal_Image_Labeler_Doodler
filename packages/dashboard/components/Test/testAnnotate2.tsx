05136109208f080c52773c1220dcbf0928c46967
Nafis <nafis.shah@gmail.com>
2021-01-02 15:40:10 -0500

    canvas brush lines, cleanup

--- packages/dashboard/components/Test/testAnnotate2.tsx
+++ packages/dashboard/components/Test/testAnnotate2.tsx
@@ -2,35 +2,69 @@ import { useEffect, useRef, useState } from 'react'
 import CanvasDraw from 'react-canvas-draw'
 import Skeleton from '@material-ui/lab/Skeleton'
 import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+// import Button from '@material-ui/core/Button'
 
-const testImage =
-  'https://nationalinterest.org/sites/default/files/main_images/G69%20%281%29.jpg'
+// const testImage =
+//   'https://nationalinterest.org/sites/default/files/main_images/G69%20%281%29.jpg'
 
-export default function TestAnnotate() {
+const colors = [
+  {
+    color: '#FF0000',
+    name: 'line #1',
+  },
+  {
+    color: '#00ffff',
+    name: 'line #2',
+  },
+  {
+    color: '#AABB00',
+    name: 'line #3',
+  },
+]
+
+export default function TestAnnotate(props: { drawingImage: string }) {
+  const { drawingImage } = props
+
   const [imgWidth, setImgWidth] = useState(0)
   const [imgHeight, setImgHeight] = useState(0)
-  const [imageRatio, setRatio] = useState(0)
+  const [imageRatio, setRatio] = useState(1)
   const [imgMaxSize, setImgMaxSize] = useState(512)
+  const [brushColor, setBrushColor] = useState(colors[0].color)
+  const [brushSize, setBrushSize] = useState(12)
+  const [lazyRadius, setLazyRadius] = useState(30)
   let saveableCanvas = useRef<{ getSaveData: any }>(null)
-  let loadableCanvas = useRef<{ getSaveData: any }>(null)
+  // let loadableCanvas = useRef<{ getSaveData: any }>(null)
+
+  function updateImageRatio(
+    height: number,
+    width: number,
+    imgMaxAxisSize: number
+  ) {
+    if (height <= 0 || width <= 0 || imgMaxAxisSize <= 0) {
+      return
+    }
+
+    let ratio = imageRatio ?? 1
+    if (height > width) {
+      ratio = imgMaxAxisSize / height
+    } else {
+      ratio = imgMaxAxisSize / width
+    }
+    setRatio(ratio)
+
+    // setImgWidth(ratio * width)
+    // setImgHeight(ratio * height)
+  }
+
   useEffect(() => {
     const img = new Image()
-    img.src = testImage
+    img.src = drawingImage
     img.onload = () => {
-      //console.log(img.height,img.width)
-      let tempRatio = 0
-      if (img.height > img.width) {
-        setRatio(imgMaxSize / img.height)
-        tempRatio = imgMaxSize / img.height
-      } else {
-        setRatio(imgMaxSize / img.width)
-        tempRatio = imgMaxSize / img.width
-      }
-
-      setImgWidth(tempRatio * img.width)
-      setImgHeight(tempRatio * img.height)
+      updateImageRatio(img.height, img.width, imgMaxSize)
+      setImgWidth(img.width)
+      setImgHeight(img.height)
     }
+    console.log(saveableCanvas)
   }, [])
 
   return (
@@ -39,39 +73,17 @@ export default function TestAnnotate() {
         <Skeleton variant="rect" width={512} height={512} />
       ) : (
         <div>
-          {imageRatio}
           <CanvasDraw
-            canvasWidth={imgWidth}
-            canvasHeight={imgHeight}
+            canvasWidth={imgWidth * imageRatio}
+            canvasHeight={imgHeight * imageRatio}
             loadTimeOffset={10}
-            imgSrc={testImage}
+            imgSrc={drawingImage}
             ref={(canvasDraw) => (saveableCanvas = canvasDraw)}
-            hideGrid={false}
-            gridColor="rgba(150,150,150,1)"
+            brushColor={brushColor}
+            lazyRadius={lazyRadius}
+            brushRadius={brushSize}
           />
-          <Button
-            onClick={() => {
-              localStorage.setItem(
-                'savedDrawing',
-                //@ts-ignore
-                saveableCanvas.getSaveData()
-              )
-            }}
-          >
-            Save
-          </Button>
-          <Button
-            onClick={() => {
-              //@ts-ignore
-              localStorage.setItem(
-                'savedDrawing',
-                //@ts-ignore
-                ''
-              )
-            }}
-          >
-            Clear
-          </Button>
+
           <TextField
             id="imgSizer"
             type="number"
@@ -80,38 +92,42 @@ export default function TestAnnotate() {
             onChange={(event) => {
               const val = parseInt(event.target.value)
               setImgMaxSize(parseInt(event.target.value))
-
-              let tempRatio = 0
-              if (imgHeight > imgWidth) {
-                setRatio(val / imgHeight)
-                tempRatio = val / imgHeight
-              } else {
-                setRatio(val / imgWidth)
-                tempRatio = val / imgWidth
-              }
-
-              setImgWidth(tempRatio * imgWidth)
-              setImgHeight(tempRatio * imgHeight)
+              updateImageRatio(imgHeight, imgWidth, val)
             }}
           />
-          <Button
-            onClick={() => {
-              //@ts-ignore
-              loadableCanvas.loadSaveData(localStorage.getItem('savedDrawing'))
+          <TextField
+            id="brushSizer"
+            type="number"
+            helperText="Size of brush"
+            defaultValue={brushSize}
+            onChange={(event) => {
+              setBrushSize(parseInt(event.target.value) ?? 12)
             }}
-          >
-            Load
-          </Button>
-          <CanvasDraw
-            disabled
-            hideGrid
-            ref={(canvasDraw) => (loadableCanvas = canvasDraw)}
-            saveData={
-              localStorage.getItem('savedDrawing') !== undefined
-                ? localStorage.getItem('savedDrawing')
-                : null
-            }
           />
+          <TextField
+            id="lazySizer"
+            type="number"
+            helperText="Size of lazy radius"
+            defaultValue={lazyRadius}
+            onChange={(event) => {
+              setLazyRadius(parseInt(event.target.value) ?? 30)
+            }}
+          />
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={`color-${index}`}
+                onClick={() => {
+                  setBrushColor(color.color)
+                }}
+                style={{
+                  color: color.color,
+                }}
+              >
+                {color.name}
+              </button>
+            )
+          })}
         </div>
       )}
     </div>
--- packages/dashboard/pages/auth/label/[imageid].tsx
+++ packages/dashboard/pages/auth/label/[imageid].tsx
@@ -2,19 +2,9 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import Layout from '@/components/Layout/index'
 import { navigationItems } from '@/components/Constants/navigation'
-// import { themeLight } from '@/components/Theme/index'
-// import { ThemeProvider } from '@material-ui/core/styles'
 import dynamic from 'next/dynamic'
 
-// const testImage =
-//   'https://nationalinterest.org/sites/default/files/main_images/G69%20%281%29.jpg'
-
-// const DynamicComponentWithNoSSR = dynamic(
-//   () => import('../../../components/Test/testAnnotate'),
-//   { ssr: false }
-// )
-
-const DynamicComponentWithNoSSR2 = dynamic(
+const TestDoodle = dynamic(
   () => import('../../../components/Test/testAnnotate2'),
   { ssr: false }
 )
@@ -27,11 +17,11 @@ export default function Home() {
     <Layout drawer navItems={navigationItems.landingPage}>
       <div>Label image {imageid}</div>
       <div>
-        {/* <ThemeProvider theme={themeLight}>
-          <DynamicComponentWithNoSSR />
-          
-        </ThemeProvider> */}
-        <DynamicComponentWithNoSSR2 />
+        <TestDoodle
+          drawingImage={
+            'https://nationalinterest.org/sites/default/files/main_images/G69%20%281%29.jpg'
+          }
+        />
       </div>
     </Layout>
   )
